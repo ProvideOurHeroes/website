9ffad9de769894f0aaa9c358b72544f7d1a53624
Dylan Theriot <dylantheriot@gmail.com>
2020-04-17 21:03:41 -0500

    added react dependencies and sample navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import Header from "../components/Header";
 
 const IndexPage = () => (
     <div>
+    <Header />
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
