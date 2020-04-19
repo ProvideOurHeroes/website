3229a3b41634e1751e0d6a96bf8f1b7d10317309
Cameron Brill <c00brill@gmail.com>
2020-04-18 23:16:08 -0500

    Centered current progress text for readability and consistency.

--- src/components/index/CurrentProgress.jsx
+++ src/components/index/CurrentProgress.jsx
@@ -5,21 +5,21 @@ export default function CurrentProgress() {
     return (<div>
     <section  class="current-progress">
         <div class="container">
-            <h2 class="text-left mt-0 responsive-header-text"><strong class="current-progress-header shimmer">Current Progress</strong></h2>
+            <h2 class="text-center mt-0 responsive-header-text"><strong class="current-progress-header shimmer">Current Progress</strong></h2>
             <div class="row">
-                <div class="col-lg-4 text-left">
+                <div class="col-lg-4 text-center">
                     <div class="mt-5">
                         <h1 class="responsive-header-text"><strong>1500+</strong></h1>
                         <p class="mb-0">Face Shields Shipped</p>
                     </div>
                 </div>
-                <div class="col-lg-4 text-left">
+                <div class="col-lg-4 text-center">
                     <div class="mt-5">
                         <h1 class="responsive-header-text"><strong>15+</strong></h1>
                         <p class="mb-0">Hospitals Provided For</p>
                     </div>
                 </div>
-                <div class="col-lg-4 text-left">
+                <div class="col-lg-4 text-center">
                     <div class="mt-5">
                         <h1 class="responsive-header-text"><strong>100%</strong></h1>
                         <p class="  mb-0">Of Your Donation Goes Towards PPE Manufacturing</p>
