de6f56a00fb40451e273947867243f150f0b84a2
Dylan Theriot <dylantheriot@gmail.com>
2020-04-19 00:24:33 -0500

    fixed merge errors

--- src/components/index/CurrentProgress.jsx
+++ src/components/index/CurrentProgress.jsx
@@ -5,21 +5,21 @@ export default function CurrentProgress() {
     return (<div>
     <section  className="current-progress">
         <div className="container">
-            <h2 className="text-left mt-0 responsive-header-text"><strong className="current-progress-header shimmer">Current Progress</strong></h2>
+            <h2 className="text-center mt-0 responsive-header-text"><strong className="current-progress-header shimmer">Current Progress</strong></h2>
             <div className="row">
-                <div className="col-lg-4 text-left">
+                <div className="col-lg-4 text-center">
                     <div className="mt-5">
                         <h1 className="responsive-header-text"><strong>1500+</strong></h1>
                         <p className="mb-0">Face Shields Shipped</p>
                     </div>
                 </div>
-                <div className="col-lg-4 text-left">
+                <div className="col-lg-4 text-center">
                     <div className="mt-5">
                         <h1 className="responsive-header-text"><strong>15+</strong></h1>
                         <p className="mb-0">Hospitals Provided For</p>
                     </div>
                 </div>
-                <div className="col-lg-4 text-left">
+                <div className="col-lg-4 text-center">
                     <div className="mt-5">
                         <h1 className="responsive-header-text"><strong>100%</strong></h1>
                         <p className="  mb-0">Of Your Donation Goes Towards PPE Manufacturing</p>
